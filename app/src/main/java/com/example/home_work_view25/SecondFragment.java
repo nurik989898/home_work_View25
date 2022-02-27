72ac30ccc7842fed3965c393368d970b7ed48309
Nurik <nturdukulov911@gmail.com>
2022-02-27 16:15:27 +0600

    do some country change.

--- app/src/main/java/com/example/home_work_view25/SecondFragment.java
+++ app/src/main/java/com/example/home_work_view25/SecondFragment.java
@@ -41,25 +41,39 @@ public class SecondFragment extends Fragment implements OnClick {
         int info = bundle.getInt(KEY);
         switch (info){
             case 1:
-               arrayList.add(new Country("Europe", R.drawable.eu, 1));
-                arrayList.add(new Country("Asia",R.drawable.asia,2));
-                arrayList.add(new Country("Africa",R.drawable.africa,3));
-                arrayList.add(new Country("North America",R.drawable.usa,4));
-                arrayList.add(new Country("South America",R.drawable.south_amerika,5));
+               arrayList.add(new Country("Germany", R.drawable.de_3x));
+                arrayList.add(new Country("Finland",R.drawable.fi_3x));
+                arrayList.add(new Country("France",R.drawable.fr_3x));
+                arrayList.add(new Country("Great Britain",R.drawable.gb_3x));
+                arrayList.add(new Country("Kazakhstan",R.drawable.kz_2x));
                 break;
             case 2:
-                arrayList.add(new Country("Europe", R.drawable.eu, 1));
-                arrayList.add(new Country("Asia",R.drawable.asia,2));
-                arrayList.add(new Country("Africa",R.drawable.africa,3));
-                arrayList.add(new Country("North America",R.drawable.usa,4));
-                arrayList.add(new Country("South America",R.drawable.south_amerika,5));
+                arrayList.add(new Country("Kyrgyzstan", R.drawable.kg_2x));
+                arrayList.add(new Country("Kazakhstan",R.drawable.kz_2x));
+                arrayList.add(new Country("South Korea",R.drawable.kr_2x));
+                arrayList.add(new Country("Saudi Arabia",R.drawable.sa_2x));
+                arrayList.add(new Country("Unated Arab Emirates",R.drawable.kw_2x));
                 break;
             case 3:
-                arrayList.add(new Country("Europe", R.drawable.eu, 1));
-                arrayList.add(new Country("Asia",R.drawable.asia,2));
-                arrayList.add(new Country("Africa",R.drawable.africa,3));
-                arrayList.add(new Country("North America",R.drawable.usa,4));
-                arrayList.add(new Country("South America",R.drawable.south_amerika,5));
+                arrayList.add(new Country("Saudi Arabia",R.drawable.sa_2x));
+                arrayList.add(new Country("Unated Arab Emirates",R.drawable.kw_2x));
+                arrayList.add(new Country("Congo",R.drawable.ke_2x));
+                arrayList.add(new Country("Sudan",R.drawable.cd_3x));
+                arrayList.add(new Country("Kenya",R.drawable.cm_3x));
+                break;
+            case 4:
+                arrayList.add(new Country("USA",R.drawable.us_3x));
+                arrayList.add(new Country("Canada",R.drawable.ca_3x));
+                arrayList.add(new Country("Alaska",R.drawable.us_3x));
+                arrayList.add(new Country("Venesuala",R.drawable.ve_3x));
+                arrayList.add(new Country("Cuba",R.drawable.ws_3x));
+                break;
+            case 5:
+                arrayList.add(new Country("Venesuala",R.drawable.ve_3x));
+                arrayList.add(new Country("Cuba",R.drawable.ws_3x));
+                arrayList.add(new Country("Dominikano",R.drawable.vn_3x));
+                arrayList.add(new Country("St.Icland",R.drawable.st_3x));
+                arrayList.add(new Country("Mexico",R.drawable.cm_3x));
                 break;
 
         }
